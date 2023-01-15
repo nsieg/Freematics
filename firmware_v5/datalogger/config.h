1c1dc7f0701c114683365492b53b9120b3928a7b
nsieg <n.siegfried@gmx.de>
2023-01-15 12:29:04 +0100

    Move custom config in separate file

--- firmware_v5/datalogger/config.h
+++ firmware_v5/datalogger/config.h
@@ -1,3 +1,5 @@
+#include "custom.h"
+
 #define MEMS_DISABLED 0
 #define MEMS_ACC 1
 #define MEMS_9DOF 2
@@ -21,13 +23,13 @@
 * WIFI and HTTP server
 **************************************/
 #ifndef HAVE_CONFIG
-#define ENABLE_HTTPD 0
+#define ENABLE_HTTPD CUSTOM_ENABLE_HTTPD
 #define ENABLE_WIFI_AP 0
-#define ENABLE_WIFI_STATION 0
+#define ENABLE_WIFI_STATION CUSTOM_ENABLE_WIFI_STATION
 #define WIFI_AP_SSID "DATALOGGER"
 #define WIFI_AP_PASSWORD "PASSWORD"
-#define WIFI_SSID "FREEMATICS"
-#define WIFI_PASSWORD "..."
+#define WIFI_SSID CUSTOM_WIFI_SSID
+#define WIFI_PASSWORD CUSTOM_WIFI_PASSWORD
 #endif
 
 #define WIFI_JOIN_TIMEOUT 30000
@@ -39,9 +41,9 @@
 **************************************/
 #ifndef HAVE_CONFIG
 // enable(1)/disable(0) OBD-II reading
-#define USE_OBD 1
+#define USE_OBD CUSTOM_USE_OBD
 // GNSS option: 0:disable 1:internal 2:external 3:SIM5360/7600 4:SIM7070
-#define USE_GNSS 2
+#define USE_GNSS CUSTOM_USE_GNSS
 // enable(1)/disable(0) MEMS motion sensor
 #define USE_MEMS 1
 #endif
