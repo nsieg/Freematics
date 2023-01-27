65ba75984ff344efdc59e7a1220064a0d43342c6
Siegfried, N. (Nils) <Nils.Siegfried@ing.de>
2023-01-27 18:07:27 +0100

    Exclude unit tests from build + extend test

--- firmware_v5/datalogger/unittest/hello_test.cc
+++ firmware_v5/datalogger/unittest/hello_test.cc
@@ -2,17 +2,56 @@
 #include "../list.cpp"
 #include <iostream>
 
-TEST(BeaconList, ShouldInit) {
+TEST(BeaconList, ShouldThrowOnWrongIndex) {
   BeaconList bl = BeaconList(5);
-  
 
-  Beacon first = bl.get(0);
-  Beacon empty = Beacon();
+  EXPECT_THROW(bl.get(0), std::out_of_range);
+  EXPECT_THROW(bl.get(1), std::out_of_range);
+  EXPECT_THROW(bl.get(5), std::out_of_range);
+  EXPECT_THROW(bl.get(6), std::out_of_range);
+}
 
-  //std::cout << "Willkommen" << first;
+TEST(BeaconList, ShouldReturnInitialized) {
+  BeaconList bl = BeaconList(5);
+  Beacon b = Beacon("1A:2B", -50);
+  bl.add(b);
 
-  std::cout << "Willkommen" << bl.size();
+  EXPECT_EQ(bl.get(0), b);
+}
 
-  EXPECT_EQ(bl.size(), 0);
-  EXPECT_EQ(first, empty);
+TEST(BeaconList, ShouldReturnSorted) {
+  BeaconList bl = BeaconList(5);
+  BeaconList bl2 = BeaconList(5);
+  Beacon far = Beacon("1A:2B", -50);
+  Beacon near = Beacon("1A:2B:3C", -10);
+  bl.add(far);
+  bl.add(near);
+  bl2.add(near);
+  bl2.add(far);
+
+  EXPECT_EQ(bl.get(0), far);
+  EXPECT_EQ(bl.get(1), near);
+  EXPECT_EQ(bl2.get(0), far);
+  EXPECT_EQ(bl2.get(1), near);
+}
+
+TEST(BeaconList, ShouldNotAddDuplicate) {
+  BeaconList bl = BeaconList(5);
+  Beacon one = Beacon("1A:2B", -50);
+  Beacon two = Beacon("1A:2B", -50);
+  bl.add(one);
+  bl.add(two);
+
+  EXPECT_EQ(bl.size(), 1);
+}
+
+TEST(BeaconList, ShouldUpdate) {
+  BeaconList bl = BeaconList(5);
+  Beacon one = Beacon("1A:2B", -50);
+  Beacon two = Beacon("1A:2B", -60);
+  bl.add(one);
+  bl.add(two);
+
+  EXPECT_EQ(bl.size(), 1);
+  EXPECT_EQ(bl.get(0), two);
 }
